ad4652f6efed3166d9d8a61a3b81e16f0176e901
9mmhp <kjop911@naver.com>
2022-12-20 15:17:19 +0900

    20221220

--- syntax/main.tf
+++ syntax/main.tf
@@ -1,12 +1,2 @@
 provider "local" {
 }
-
-variable "number" {
-  description = "An example of a variable in Terraform"
-  type        = number
-  default     = 42
-}
-
-output "number_example" {
-  value = var.number_example
-}
